794952644e03c4a2b0db3ca22c6b8a04541e622a
Kevin Ahn <kevinahn7@hotmail.com>
2018-08-31 12:57:41 -0700

    Add preliminary styling

--- src/app/models/projects.model.ts
+++ src/app/models/projects.model.ts
@@ -5,5 +5,26 @@ export let Projects: object[] = [
         image: "assets/beat-maker.PNG",
         skills: ["HTML", "CSS", "JavaScript"],
         description: "An app to make beats"
+    },
+    {
+        name: "Beat Maker",
+        url: "https://kevinahn7.github.io/beat-maker/",
+        image: "assets/beat-maker.PNG",
+        skills: ["HTML", "CSS", "JavaScript"],
+        description: "An app to make beats"
+    },
+    {
+        name: "Beat Maker",
+        url: "https://kevinahn7.github.io/beat-maker/",
+        image: "assets/beat-maker.PNG",
+        skills: ["HTML", "CSS", "JavaScript"],
+        description: "An app to make beats"
+    },
+    {
+        name: "Beat Maker",
+        url: "https://kevinahn7.github.io/beat-maker/",
+        image: "assets/beat-maker.PNG",
+        skills: ["HTML", "CSS", "JavaScript"],
+        description: "An app to make beats"
     }
 ]
