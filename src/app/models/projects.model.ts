3315d872894f9998ef6081b42d64f24335335f45
Kevin Ahn <kevinahn7@hotmail.com>
2018-09-03 14:51:52 -0700

    Add whos-that-pokemon project to portfolio

--- src/app/models/projects.model.ts
+++ src/app/models/projects.model.ts
@@ -1,30 +1,16 @@
 export let Projects: object[] = [
     {
-        name: "Beat Maker",
-        url: "https://kevinahn7.github.io/beat-maker/",
-        image: "assets/beat-maker.PNG",
-        skills: ["HTML", "CSS", "JavaScript"],
-        description: "An app to make beats"
+        name: "Who's That Pokémon",
+        url: "https://whos-that-pokemon-57ff9.firebaseapp.com/",
+        image: "assets/whos-that-pokemon.PNG",
+        skills: ["HTML", "SASS", "JavaScript", "Angular", "Pokéapi"],
+        description: "An app to play 'Who's That Pokémon', based on the Pokémon show bumper"
     },
     {
         name: "Beat Maker",
         url: "https://kevinahn7.github.io/beat-maker/",
         image: "assets/beat-maker.PNG",
         skills: ["HTML", "CSS", "JavaScript"],
-        description: "An app to make beats"
-    },
-    {
-        name: "Beat Maker",
-        url: "https://kevinahn7.github.io/beat-maker/",
-        image: "assets/beat-maker.PNG",
-        skills: ["HTML", "CSS", "JavaScript"],
-        description: "An app to make beats"
-    },
-    {
-        name: "Beat Maker",
-        url: "https://kevinahn7.github.io/beat-maker/",
-        image: "assets/beat-maker.PNG",
-        skills: ["HTML", "CSS", "JavaScript"],
-        description: "An app to make beats"
+        description: "A simple app to create, listen to and save local beats"
     }
 ]
